b3201761a15410e7b27036070f6544825b284177
Alejandro <a.bazagavi@gmail.com>
2020-04-06 12:34:36 +0200

    Módulo de Odoo 10

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'School',
+    'version': '4.0',
+    'summary': 'School Management',
+    'description': """
+          Module prepared by department "Informàtica i comunicacions"
+          of Institute Milà i Fontanals in Igualada (Barcelona-Spain)
+          for learning in development and adaptation of modules of Odoo ERP.          
+
+          It is part of the learning materials for the module
+          "Sistemes de gestió empresarial" in the course
+          "CFS Desenvolupament d'aplicacions multiplataforma".
+    """,
+    'author': 'Group DAM2 - Course 2019-2020',
+    'website': 'http://www.infomila.info',
+    'depends': ['base'],
+    'data': ['views/school_views.xml'],
+    'demo': [],
+    'installable': True,
+    'application': False,
+}
---
+++ models/school.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Teacher(models.Model):
+  _name = 'school.teacher'
+  _description = 'Teacher Management'
+  _rec_name = 'last_name'
+
+  first_name = fields.Char('First Name',size=30,required=True)
+  last_name = fields.Char('Last Name',size=40,required=True)
+  birthdate = fields.Date('Birthdate',required=True)
+  tin = fields.Char('Tax ID',size=14,required=True)
+  gender = fields.Selection([('male','Male'),('female','Female')],'Gender')
+  salary = fields.Integer('Salary')
+  email = fields.Char('eMail',size=60,required=True)
+  phone = fields.Char('Phone')
+  active = fields.Boolean('Active?',default=True)
+  course_ids = fields.One2many('school.course','manager_id','Courses managed',
+                               readonly=True)
+  subject_ids = fields.Many2many('school.subject','school_subject_teacher_rel','teacher_id','subject_id',
+                                 'Subjects authorized',readonly=True)
+
+class Course(models.Model):
+  _name = 'school.course'
+  _description = 'Course Management'
+  
+  name = fields.Char('Name',size=60,required=True)
+  hours = fields.Integer('Hours',required=True)
+  active = fields.Boolean('Active?',default=True)
+  manager_id = fields.Many2one('school.teacher','Teacher Manager')
+  subject_ids = fields.Many2many('school.subject','school_course_subject_rel',
+                                 'course_id','subject_id','Subjects')
+
+class Subject(models.Model):
+  _name = 'school.subject'
+  _description = 'Subject Management'
+  
+  name = fields.Char('Name',size=60,required=True)
+  hours = fields.Integer('Hours',required=True)
+  active = fields.Boolean('Active?',default=True)
+  course_ids = fields.Many2many('school.course','school_course_subject_rel',
+                                'subject_id','course_id','Courses')
+  teacher_ids = fields.Many2many('school.teacher','school_subject_teacher_rel','subject_id','teacher_id',
+                                 'Authorized Teachers',readonly=True)
+
